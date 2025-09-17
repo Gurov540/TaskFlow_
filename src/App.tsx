d19910ff54f3180f2a5b3d8e443c25de84f0275e
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-09-17 23:19:19 +0300

    recreated Vite build

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/icons/react.svg";
-import viteLogo from "./assets/icons/vite.svg";
 import "./App.css";
-import { NavBar } from "@/widgets/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <main className="p-6">
-        <Routes>
-          <Route path="/" element={<h1>Главная</h1>} />
-          <Route path="/about" element={<h1>О проекте</h1>} />
-          <Route path="/contact" element={<h1>Контакты</h1>} />
-        </Routes>
-      </main>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </BrowserRouter>
-  );
+  return <></>;
 }
 
 export default App;
