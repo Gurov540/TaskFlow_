e36fc224319ddd8b15e279dd6e3448059bd7db01
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-09-17 23:32:59 +0300

    fix штсщккусе

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import logo from "../../../assets/logo/react.svg";
+import logo from "../../../assets/icons/react.svg";
 
 export const Logo: React.FC = () => {
   return <img className={styles.header} src={logo} alt="Logo" />;
