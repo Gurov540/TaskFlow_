3c079fb00edfc5ae162629bda4693eb4dc1ad78f
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-09-19 23:46:15 +0300

    add page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { Header } from "./widgets/Header";
+import { GeneralPage } from "./pages/GeneralPage";
+import { LogRegPage } from "./pages/LogRegPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<GeneralPage />} />
+        <Route path="/about" element={<LogRegPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
