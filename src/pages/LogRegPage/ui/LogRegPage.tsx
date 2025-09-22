a558a2eb3e1272c66132168152db12bf160249c4
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-09-22 22:34:33 +0300

    add log/reg page

--- src/pages/LogRegPage/ui/LogRegPage.tsx
+++ src/pages/LogRegPage/ui/LogRegPage.tsx
@@ -1,3 +1,52 @@
+import "./LogReg.model.css";
+
 export const LogRegPage = () => {
-  return <h1>LogReg страница</h1>;
+  return (
+    <section
+      id="loginModal"
+      className="modal active"
+      aria-hidden="true"
+      aria-labelledby="loginTitle"
+    >
+      <div className="modal-content">
+        <span className="close">
+          <i className="fas fa-times"></i>
+        </span>
+        <h2 className="form-title">LogIn</h2>
+        <div className="modalTabs">
+          <button className="modalTab-button active">Login</button>
+          <button className="modalTab-button">Register</button>
+        </div>
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              id="login-email"
+              name="email"
+              required
+              aria-required="true"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              aria-required="true"
+            />
+          </div>
+
+          <div className="form-actions">
+            <button>Cancel</button>
+            <button id="modal-log-btn" className="modal-btn action-btn">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
