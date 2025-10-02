3fb22a532584810c611c2e8921256440666ada8c
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-10-02 16:58:23 +0300

    change: adaptive to modal use

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { Header } from "./widgets/Header";
 import { GeneralPage } from "./pages/GeneralPage";
-import { LogRegPage } from "./pages/LogRegPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<GeneralPage />} />
-        <Route path="/about" element={<LogRegPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Header />
+    <GeneralPage></GeneralPage>
   </StrictMode>
 );
