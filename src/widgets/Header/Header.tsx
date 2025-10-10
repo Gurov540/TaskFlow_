4203c7e75d8140adfe59b3198683e7bf89b669ae
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-10-10 21:05:09 +0300

    delete Modal open function

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import styles from "./Header.module.css";
 import { Button } from "../../shared/ui/Button";
 import { Logo } from "../../shared/ui/Logo";
-import { useAuthModal } from "../../shared/lib/hooks/useAuthModal";
-import { AuthModal } from "../../shared/ui/AuthModal";
 
 export const Header: React.FC = () => {
-  const authModal = useAuthModal();
-
   return (
     <>
       <header className={styles.header}>
@@ -15,29 +11,14 @@ export const Header: React.FC = () => {
           <Logo />
         </div>
         <nav className={styles.nav}>
-          <Button
-            color="primary"
-            size="medium"
-            onClick={authModal.openLoginModal}
-          >
+          <Button color="primary" size="medium">
             Login
           </Button>
-          <Button
-            color="success"
-            size="medium"
-            onClick={authModal.openRegisterModal}
-          >
+          <Button color="success" size="medium">
             Registration
           </Button>
         </nav>
       </header>
-
-      <AuthModal
-        isOpen={authModal.isAuthModalOpen}
-        onClose={authModal.closeAuthModal}
-        mode={authModal.authMode}
-        onSwitchMode={authModal.switchAuthMode}
-      />
     </>
   );
 };
