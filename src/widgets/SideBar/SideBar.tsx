ab716a6b91cf774bf6c4401a5d6515809152470c
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-10-10 21:06:06 +0300

    add new tabs

--- src/widgets/SideBar/SideBar.tsx
+++ src/widgets/SideBar/SideBar.tsx
@@ -10,36 +10,13 @@ export const SideBar: React.FC = () => {
     { label: "Boards", icon: <Icon name="layout-dashboard" /> },
     { label: "Favorites", icon: <Icon name="star" /> },
     { label: "Activity Log", icon: <Icon name="history" /> },
-  ];
-
-  const recentBoards = [
-    {
-      id: 1,
-      name: "Work Tasks",
-      icon: <Icon name="briefcase" />,
-      tasks: 3,
-      updated: "Updated today",
-    },
-    {
-      id: 2,
-      name: "Fitness Goals",
-      icon: <Icon name="activity" />,
-      tasks: 5,
-      updated: "Updated yesterday",
-    },
+    { label: "Hui", icon: <Icon name="trash" /> },
+    { label: "Time", icon: <Icon name="pen" /> },
   ];
 
   return (
     <aside className={styles.sidebar}>
-      {/* Верхняя часть */}
       <div>
-        {/* Текущая доска */}
-        <div className={styles.currentBoard}>
-          <Icon name="folder" />
-          <span>Melon Board</span>
-        </div>
-
-        {/* Навигация */}
         <nav className={styles.nav}>
           {navTabs.map((tab) => (
             <Tab
@@ -51,26 +28,6 @@ export const SideBar: React.FC = () => {
             />
           ))}
         </nav>
-      </div>
-
-      {/* Недавние доски */}
-      <div className={styles.recent}>
-        <h4 className={styles.recentTitle}>Recent Boards</h4>
-        <div className={styles.recentList}>
-          {recentBoards.map((board) => (
-            <div key={board.id} className={styles.recentItem}>
-              <div className={styles.recentInfo}>
-                <span className={styles.icon}>{board.icon}</span>
-                <div>
-                  <p className={styles.boardName}>{board.name}</p>
-                  <p className={styles.boardMeta}>
-                    {board.tasks} tasks • {board.updated}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </aside>
   );
